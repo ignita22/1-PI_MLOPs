5d35e1ada7f224635afc49352ed4780b157b574a
ignita22 <123280966+ignita22@users.noreply.github.com>
2023-12-20 17:48:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -32,13 +32,34 @@ async def PlayTimeGenre(genre: str):
 @app.get('/UserForGenre/{genre}')
 
 async def UserForGenre(genre: str):
+    try:
+        # Filtrar por el género específico en el DataFrame tabla
+        df_genero = tabla[tabla['genres'].apply(lambda x: genre in x)]
+        df_usuario = df_user_items_explode[df_user_items_explode['item_name'].str.contains(genre, case=False)]
 
-    genre_data = tabla[tabla['genres'].str.contains(genre, case=False)]
+        if df_genero.empty or df_usuario.empty:
+            # Si alguno de los DataFrames está vacío, lanzar un error ValueError
+            raise ValueError("No hay juegos o usuarios en el género especificado")
 
-    max_playtime_users = genre_data.groupby('genres').apply(lambda x: x.loc[x['playtime_forever'].idxmax()]['user_id']).reset_index(name='user_id')
+        # Encontrar el usuario con más horas jugadas para el género específico
+        usuario_mas_horas = str(df_usuario.loc[df_usuario['playtime_forever'].idxmax()]['user_id'])
 
-    playtime_by_year = genre_data.groupby(['genres', 'year'])['playtime_forever'].sum().reset_index()
+        # Obtener el total de horas jugadas por el usuario en ese género
+        horas_mas_usuario = str(df_usuario['playtime_forever'].max())
 
-    return max_playtime_users, playtime_by_year
+        # Calcular la acumulación de horas jugadas por año para ese género
+        acumulacion_horas_por_año = df_genero.groupby('year')['playtime_forever'].sum().reset_index()
+        lista_acumulacion = [{"Año": int(año), "Horas": horas} for año, horas in zip(
+            acumulacion_horas_por_año['year'], acumulacion_horas_por_año['playtime_forever'])]
 
+        # Crear el diccionario de retorno con la información recopilada
+        resultado = {
+            "Usuario con más horas jugadas para Género " + genre: usuario_mas_horas,
+            "Total de horas jugadas por el usuario": horas_mas_usuario,
+            "Acumulación de horas jugadas por año": lista_acumulacion}
+
+        return resultado  # Devolver el diccionario con la información solicitada
+    except Exception as e:
+        # Si hay cualquier otro tipo de excepción, lanzar un error ValueError
+        raise ValueError(f"Error interno: {str(e)}")
 
