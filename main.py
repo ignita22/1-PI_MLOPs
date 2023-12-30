27900c09d00354ba217a7ebbdaff6efc2d941df1
ignita22 <123280966+ignita22@users.noreply.github.com>
2023-12-30 12:50:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def UsersRecommend(year: int):
     try: 
         
         # Convertir la columna 'fecha' a datetime si no está en ese formato
-        df_user_reviews_final['fecha'] = pd.to_datetime(user_reviews_final['fecha'], errors='coerce')
+        user_reviews_final['fecha'] = pd.to_datetime(user_reviews_final['fecha'], errors='coerce')
     
         # Filtrar por el año especificado
         df_specific_year = user_reviews_final[user_reviews_final['fecha'].dt.year == year]
